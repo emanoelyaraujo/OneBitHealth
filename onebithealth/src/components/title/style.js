89aa430d89566fd50ec3fae668897b4e5a7bd63d
Emanoely Araújo <emanoelyaraujo004@gmail.com>
2022-03-13 21:59:50 -0300

    feat: title components and styles

---
+++ onebithealth/src/components/title/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    boxTitle: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 15
+    },
+    textTitle:{
+        color: "#FF1493",
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+    textSubtitle: {
+        fontSize: 15,
+        fontWeight: "bold"
+    }
+});
+
+export default styles
